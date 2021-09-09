bda23e43e0cd739062004a1fff16c071dea23915
thuccv98 <thuccv98@gmail.com>
2021-09-09 10:57:58 +0700

    Create login and register api backend

---
+++ backend/controllers/userController.js
@@ -0,0 +1,51 @@
+const asyncHandler = require('express-async-handler');
+const User = require('../models/userModel');
+
+// handle register
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  // check user da ton tai trong database hay chua
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error('User Already Exists');
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Error Occurred');
+  }
+});
+
+// handle login
+const authUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid Email or Password!');
+  }
+});
+
+module.exports = { registerUser, authUser };
--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,14 @@ const express = require('express');
 const notes = require('./data/notes');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const userRouter = require('./routers/userRouter');
+const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
 
 const app = express();
 dotenv.config();
 
 connectDB();
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('api is running..');
@@ -15,6 +18,11 @@ app.get('/', (req, res) => {
 app.get('/api/data', (req, res) => {
   res.json(notes);
 });
+
+app.use('/api/users', userRouter);
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
